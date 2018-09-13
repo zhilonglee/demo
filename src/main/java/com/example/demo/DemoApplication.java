8251b6ce9c88fdf4d822c64373147079977761c4
lizhilong <zhilong.li@ncs.com.sg>
2018-09-13 22:10:29 +0800

    Test for resolving conflict.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class DemoApplication {
 
-//test  for push   by hjb at 2018/09/13
+// test  for push   by hjb at 2018/09/13.
+// test for resolving confilc.
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
