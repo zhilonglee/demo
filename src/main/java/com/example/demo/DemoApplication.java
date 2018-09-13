c82c23cdd4de64668a9bbf7c588b6ae5af68c650
GitUserHu <1055174133@qq.com>
2018-09-13 22:15:47 +0800

    one more test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class DemoApplication {
-
-//test  for push   by hjb at 2018/09/13
+//add for testing by hjb at 2018/09/13  --for one more time.   22:00
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
